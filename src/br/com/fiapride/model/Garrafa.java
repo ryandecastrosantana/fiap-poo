d90a85e5811112e5899583befc92a004b1dabae9
Ryan Amorim De Castro Santana <ryandecastrosantana@gmail.com>
2026-03-09 00:35:06 -0300

    docs: adiciona diagrama de classes

---
+++ src/br/com/fiapride/model/Garrafa.java
@@ -0,0 +1,8 @@
+package br.com.fiapride.model;
+public class Garrafa { // <-- Use o nome do SEU objeto    
+    // As características que você pensou    
+    public String cor;
+    public String material;
+    public int capacidadeEmMl;
+    
+}
